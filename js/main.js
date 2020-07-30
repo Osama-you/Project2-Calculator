ca62a3ace214b7a3a98f2e96c17011cfd36bbdd1
Osama-you <osama.w.younis@gmail.comm>
2020-07-30 09:27:58 +0300

    the function for Basic operations is done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 // js file for Project2-Calculator
+
+//Basic operations
+let add = (A, B) => A + B
+let div = (A, B) => A / B
+let sub = (A, B) => A - B
+let mult = (A, B) => A * B
+
+// take to input and op then return the result as obj whit message
+let operation = (opr,A,B) => {
+    let data = {
+      result: NaN,
+      mes: ""
+    }
+    // Check if the input is correct
+    if (isNaN(A)||isNaN(B)){
+      data.mes = "The input is wrong" 
+      return data
+    }
+    // Convert to number if it string
+    A = parseFloat(A)
+    B = parseFloat(B)
+
+    // Select the operation
+    switch(opr) {
+      case "+":
+        data.result = add(A, B)
+        break
+      case "/":
+        data.result = div(A, B)
+        break
+      case "-":
+        data.result = sub(A, B)
+        break
+      case "*":
+        data.result = mult(A, B)
+        break
+      default:
+        data.mes="The operation is wrong"
+    }
+    return data
+  }
